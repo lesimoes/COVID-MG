e374c8df6883a012c44aff0bf63dd876b24cc420
lesimoes <lesimoes0@gmail.com>
2020-06-17 22:46:41 -0300

    1- Fixed with await pipe stream. 2- added mock

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const settings = require('../config/settings');
 const Scheduler = require('./functions/Scheduler');
 const S3Listener = require('./functions/S3Listener');
+const SQSListener = require('./functions/SQSListener');
 
 const scheduler = new Scheduler(settings);
 const s3listener = new S3Listener(settings);
+const sqslistener = new SQSListener(settings);
 
 module.exports = {
   scheduler: scheduler.main.bind(scheduler),
   s3listener: s3listener.main.bind(s3listener),
+  sqslistener: sqslistener.main.bind(sqslistener),
 }
